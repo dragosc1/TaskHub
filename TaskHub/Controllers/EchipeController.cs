e44bf2c2d7917411a0519a79e5ccf935672ae371
dragosc1 <dragos.ciobanu232@gmail.com>
2023-12-19 00:34:00 +0200

    Added echipa CRUD

--- TaskHub/Controllers/EchipeController.cs
+++ TaskHub/Controllers/EchipeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskHub.Database;
+using TaskHub.Models;
 
 namespace TaskHub.Controllers
 {
@@ -22,8 +23,51 @@ namespace TaskHub.Controllers
         {
             var currentUserId = (await _userManager.GetUserAsync(User)).Id;
             var Echipe = _db.Echipe.Where(e => e.IdUtilizator == currentUserId).Include("Proiect");
-            ViewBag.Echipe = Echipe;    
+            ViewBag.Echipe = Echipe;   
             return View();
+        }
+        [Authorize(Roles ="organizator,administrator,membru")]
+        public async Task<IActionResult> Show(int? Id)
+        {
+            var Proiect = _db.Proiecte.FirstOrDefault(p => p.Id == Id);
+            ViewBag.Proiect = Proiect;
+            var MembriiId = await _db.Echipe
+            .Where(e => e.IdProiect == Id)
+            .Select(e => e.IdUtilizator)
+            .ToListAsync();
+
+            var Membrii = await System.Threading.Tasks.Task.WhenAll(MembriiId.Select(id => _userManager.FindByIdAsync(id)));
+            var EmailAddresses = Membrii.Select(user => user?.Email).ToList();
+
+            bool Organizator;
+            if (User.IsInRole("organizator"))
+                Organizator = true;
+            else Organizator = false;
+            ViewBag.Membrii = Membrii;
+            ViewBag.Organizator = Organizator;  
+            return View();
+        }
+        [HttpPost]
+        [Route("Echipe/Delete/{nume?}/{idProiect}")]
+        [Authorize(Roles = "organizator")]
+        public async Task<IActionResult> Delete(string? nume, int idProiect)
+        {
+            var user = await _userManager.FindByEmailAsync(nume);
+
+            if (user == null)
+            {
+                // Handle the case where the user is not found
+                // You might want to return a specific view or a NotFound result
+                return NotFound();
+            }
+
+            Echipa echipa = new Echipa { IdProiect = idProiect, IdUtilizator = user.Id };
+            _db.Echipe.Remove(echipa);
+
+            await _db.SaveChangesAsync();
+
+            // Redirect to the Index action after successful deletion
+            return RedirectToAction("Index");
         }
     }
 }
